fda87b94fdb9d37337c64ca20700af061b0c9fd2
vangheem <vangheem@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-12-01 06:45:48 +0000

    handle error with plone.app.async jobs

--- wc/pageturner/events.py
+++ wc/pageturner/events.py
@@ -2,6 +2,9 @@ from Products.CMFCore.utils import getToolByName
 from convert import convert
 from zope.component import getUtility
 from settings import Settings
+from logging import getLogger
+
+logger = getLogger('wc.pageturner')
 
 try:
     from plone.app.async.interfaces import IAsyncService
@@ -21,9 +24,13 @@ def handle_file_creation(object, event):
         object.setLayout('page-turner')
         
     if async_installed:
-        settings = Settings(object)
-        async = getUtility(IAsyncService)
-        job = async.queueJob(convert, object)
-        settings.converting = True
+        try:
+            settings = Settings(object)
+            async = getUtility(IAsyncService)
+            job = async.queueJob(convert, object)
+            settings.converting = True
+        except:
+            logger.exception("Error using plone.app.async with wc.pageturner. Converting pdf without plone.app.async...")
+            convert(object)
     else:
         convert(object)
